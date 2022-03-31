7fcb54268a5c06f9973872e3e0b0fd1c7ab55bcf
maya <C00217557@itcarlow.ie>
2022-03-31 11:04:26 +0100

    testing

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
-from flask import Flask, request, render_template
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 
 app = Flask(__name__)
 
-@app.get("/")
+@app.route("/")
 def index(request):
     return render_template("mainMenu.html",title="Hexapawn")
 
-@app.get("/singleplayer")
-def singlePlayer(request):
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon') 
+
+@app.route("/singleplayer")
+def singlePlayer():
     return render_template("singleplayer.html",title="Hexapawn")
 
-@app.get("/multiplayer")
-def multiplayer(request):
-    return render_template("singleplayer.html",title="Hexapawn")
+@app.route("/multiplayer")
+def multiplayer():
+    return render_template("multiplayer.html",title="Hexapawn")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
